0fb64d7d78a35017eae8735eba7ede1fd5905868
Bhoomit Patel <bhoomit.patel@logicalwings.com>
2020-02-19 11:56:58 +0530

    Done

--- app/src/main/java/com/example/bottomsheet/data/Data.kt
+++ app/src/main/java/com/example/bottomsheet/data/Data.kt
@@ -1,11 +1,4 @@
 package com.example.bottomsheet.data
-
 import com.example.bottomsheet.R
-
 val titleList :List<String> = listOf("Image 1","Image 2","Image 3","Image 4")
-val imageList :List<Int> = listOf(
-    R.drawable.item1,
-    R.drawable.item2,
-    R.drawable.item3,
-    R.drawable.item4
-)
+val imageList :List<Int> = listOf(R.drawable.item1, R.drawable.item2, R.drawable.item3, R.drawable.item4)
--- app/src/main/java/com/example/bottomsheet/model/Screens.kt
+++ app/src/main/java/com/example/bottomsheet/model/Screens.kt
@@ -1,15 +1,12 @@
 package com.example.bottomsheet.model
-
 import androidx.compose.Model
-
 sealed class Screens {
     object Home : Screens()
     object Second : Screens()
 }
 @Model
 object Status {
-    var currentScreen: Screens =
-        Screens.Home
+    var currentScreen: Screens = Screens.Home
 }
 fun navigateTo(destination: Screens) {
     Status.currentScreen = destination
